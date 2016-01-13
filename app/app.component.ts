1e98fb1d09348361de0a0d2b91de8c4513fb7a35
Ankur Sethi <contact@ankursethi.in>
2016-01-13 16:22:43 +0530

    Added support for adjusting sample volume.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,15 @@ import { Component, OnInit } from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div><button (click)="onClick()" [disabled]="loadingSample">play</button></div>
-        <div><input type="range" min="0.01" max="5.0" step="0.01" [(ngModel)]="playbackRate"></div>
-
+        <div><button (click)='playSample()' [disabled]='loadingSample'>play</button></div>
+        <div>
+            <label for='playbackRate'>Playback rate: </label>
+            <input type='range' min='0.01' max='5.0' step='0.01' [(ngModel)]='playbackRate' id='playbackRate'>
+        </div>
+        <div>
+            <label for='gain'>Volume: </label>
+            <input type='range' min='0' max='1.0' step='0.1' [(ngModel)]='gain' id='gain'>
+        </div>
     `
 })
 export class AppComponent implements OnInit {
@@ -14,6 +20,7 @@ export class AppComponent implements OnInit {
     private loadingSample: boolean = false;
     private audioBuffer: AudioBuffer;
     private playbackRate: number = 1.0;
+    private gain: number = 1.0;
 
     ngOnInit() {
         this.audioContext = new AudioContext();
@@ -43,7 +50,13 @@ export class AppComponent implements OnInit {
         let bufferSource = this.audioContext.createBufferSource();
         bufferSource.buffer = this.audioBuffer;
         bufferSource.playbackRate.value = this.playbackRate;
-        bufferSource.connect(this.audioContext.destination);
+
+        let gainNode = this.audioContext.createGain();
+        gainNode.gain.value = this.gain;
+
+        bufferSource.connect(gainNode);
+        gainNode.connect(this.audioContext.destination);
+
         bufferSource.start(0);
     }
 
