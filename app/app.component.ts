4cac8ed97ba2cf92b0798a149e22866a6941a070
Ankur Sethi <contact@ankursethi.in>
2016-01-13 16:19:26 +0530

    Added support for changing playback rate.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: `<button (click)="onClick()" [disabled]="loadingSample">play</button>`
+    template: `
+        <div><button (click)="onClick()" [disabled]="loadingSample">play</button></div>
+        <div><input type="range" min="0.01" max="5.0" step="0.01" [(ngModel)]="playbackRate"></div>
+
+    `
 })
 export class AppComponent implements OnInit {
     private audioContext: AudioContext;
     private loadingSample: boolean = false;
     private audioBuffer: AudioBuffer;
+    private playbackRate: number = 1.0;
 
     ngOnInit() {
         this.audioContext = new AudioContext();
@@ -37,6 +42,7 @@ export class AppComponent implements OnInit {
     playSample() {
         let bufferSource = this.audioContext.createBufferSource();
         bufferSource.buffer = this.audioBuffer;
+        bufferSource.playbackRate.value = this.playbackRate;
         bufferSource.connect(this.audioContext.destination);
         bufferSource.start(0);
     }
