273ea938d608e36896b2f1c319edf7e86a7f77e2
Ankur Sethi <contact@ankursethi.in>
2016-01-13 16:15:59 +0530

    Added support for loading and playing a single sample.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,47 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>`
+    template: `<button (click)="onClick()" [disabled]="loadingSample">play</button>`
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    private audioContext: AudioContext;
+    private loadingSample: boolean = false;
+    private audioBuffer: AudioBuffer;
+
+    ngOnInit() {
+        this.audioContext = new AudioContext();
+
+        this.loadingSample = true;
+        this.fetchSample()
+            .then((audioBuffer) => {
+                this.loadingSample = false;
+                this.audioBuffer = audioBuffer;
+            })
+            .catch((error) => {
+                throw error;
+            });
+    }
+
+    fetchSample() {
+        return fetch('samples/snare.wav')
+            .then((response) => response.arrayBuffer())
+            .then((buffer) => {
+                return new Promise((resolve, reject) => {
+                    this.audioContext.decodeAudioData(buffer, resolve, reject);
+                });
+            });
+    }
+
+    playSample() {
+        let bufferSource = this.audioContext.createBufferSource();
+        bufferSource.buffer = this.audioBuffer;
+        bufferSource.connect(this.audioContext.destination);
+        bufferSource.start(0);
+    }
+
+    onClick() {
+        this.playSample();
+    }
+}
